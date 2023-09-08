195f3dbf48cea2ebd0ffef31c07c603401ca57e7
abond4 <142537374+abond4@users.noreply.github.com>
2023-09-07 21:07:29 -0400

    Add files via upload

---
+++ Week 2/binomial-test-script_ICA.R
@@ -0,0 +1,28 @@
+#### This is a script ####
+
+# There are a few differences you want to take note of in a script
+#   1. Formatting isn't a thing, it's either code or not
+#   2. Text is made in the same way as in a markdown, with the symbol # in front
+#   3. There is no setup chunk. Imagine this is just a long version of the console
+
+
+######################################
+##    The Binomial Test             ## 
+######################################
+
+
+# 1. set working directory and load libraries
+library(tidyverse)
+
+# 2. create table with RTs in it
+
+
+# 3. calculate how many outcomes are greater than value
+
+
+# 4. calculate p value
+
+
+
+#what can we infer from this test?
+
