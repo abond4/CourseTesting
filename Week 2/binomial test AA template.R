936de0ee4c11acde2b94b86ed8a2fea0f775c40a
abond4 <142537374+abond4@users.noreply.github.com>
2023-09-11 14:42:35 -0400

    Add files via upload

---
+++ Week 2/binomial test AA template.R
@@ -0,0 +1,38 @@
+# Application Assignment #2: Binomial Test
+
+
+# PART 1
+#What test should they run (including x, size, prob in your answer)
+
+
+
+
+# PART 2
+
+# 1. Create a data frame
+
+
+
+# 2. Compare the original and new reaction times
+
+
+
+# 3. Calculate the probability
+
+
+
+# 4. Make and interpret a decision (i.e., reject or retain; what does it mean)
+
+
+
+# PART 3
+
+
+# 1. What test would you run (include x, size, prob in your answer)
+
+
+
+
+
+#2. Make and interpret a decision (i.e., reject or retain; what does it mean)
+
