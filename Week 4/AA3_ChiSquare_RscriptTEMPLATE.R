c9d47ddf04438afb50932a8c1cddc747eff1261d
abond4 <142537374+abond4@users.noreply.github.com>
2023-09-19 14:06:12 -0400

    Add files via upload

---
+++ Week 4/AA3_ChiSquare_RscriptTEMPLATE.R
@@ -0,0 +1,38 @@
+#
+# CHI SQUARE: TEST OF INDEPENDENCE
+#
+
+
+
+# build a matrix with all variables 
+
+
+
+#view matrix
+
+
+
+# add row names and column names
+
+
+
+# view matrix
+
+
+
+# Run chi-square test
+
+
+
+# Run code to review the expected values
+
+
+
+
+# What was the chi-square number? What was the p-value? 
+# (make sure you use a hashtag in front for text)
+
+
+
+
+# Do we retain or reject the null hypothesis
