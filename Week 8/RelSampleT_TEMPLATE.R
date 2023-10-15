e697ce439c472d9280b0424d89fceac9d55afe62
abond4 <142537374+abond4@users.noreply.github.com>
2023-10-15 19:44:53 -0400

    Add files via upload

---
+++ Week 8/RelSampleT_TEMPLATE.R
@@ -0,0 +1,27 @@
+# Background:  People will like a new art installment in their community less the first time they see 
+#              it than after one year of exposure. Participants rated how much they liked art work
+#              on a scale of 1-7 the first day their saw the artwork and the one year later.
+#  
+# Open the "artexposure_RelsampleT" file in your Week 8 Folder
+
+
+
+
+# 1. What is the Iv and DV?
+
+
+
+
+
+# 2. State the null and alternative hypotheses
+
+
+
+
+# 3. Run the appropriate normality tests and interpet (note: you will need the difference score)
+
+
+
+
+
+# 4. Run the appropriate test statistic 
