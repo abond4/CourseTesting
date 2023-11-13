3bf04e133589fb4a14fe9f16a01dba724cfc7fb1
abond4 <142537374+abond4@users.noreply.github.com>
2023-11-12 21:47:47 -0500

    Add files via upload

---
+++ Week 12/FactorialANOVALectureExample_TEMPLATE.R
@@ -0,0 +1,55 @@
+# load libraries
+
+library(tidyverse)
+library(rstatix)
+library(lawstat)
+library(rstatix)
+
+
+# create a dataframe 
+
+sugars <- data.frame(
+  buffet = c(rep("Absent", 18), rep("Present", 18)),
+  exposure = c(rep("Low", 6), rep("Mod", 6), rep("High", 6), rep("Low", 6), rep("Mod", 6), rep("High", 6)),
+  times = c(8, 7, 9, 10, 12, 8, 10, 12, 15, 8, 6, 9, 13, 9, 11, 8, 13, 12, 5, 8, 5, 6, 5, 7, 15, 10, 8, 9, 7, 11, 15, 12, 15, 16, 12, 14)
+)
+
+# ANVOVA model
+
+
+
+
+
+############################
+## ASSUMPTIONS
+###########################
+
+
+
+
+
+
+
+
+
+
+
+
+
+################################################
+## TEST STATISIC | INTERACTIONS | MAIN EFFECTS
+################################################
+
+
+
+
+
+#SIMPLE MAIN EFFECTS & POST HOCS FOR INTERACTION
+#################################################
+
+
+
+
+
+# POST HOCS FOR MAIN EFFECTS
+################################################
