d4aa78d436c728f369fb0f629aeb24386963eb80
abond4 <142537374+abond4@users.noreply.github.com>
2023-11-12 22:48:16 -0500

    Add files via upload

---
+++ Week 12/ChessPatterns_TEMPLATE.R
@@ -0,0 +1,6 @@
+## Open the file named "chesspatterns.cvs" 
+## Hypothesis: Chess experts can reproduce patterns of chess pieces on a board more accurately than novices, 
+##            but only if the patterns would occur in a game of chess (as oppose to a random pattern)
+## DV: Number of correctly reproduced board sequences. 
+
+## Test the hypothesis 
